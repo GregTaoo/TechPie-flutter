1cb2cf384512fe88fc0711e952de42c029f8843f
GregTao <gregtaoo@outlook.com>
2026-06-05 01:56:09 +0800

    feat: calendar subscription

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/user_session.dart';
 import 'api_base_url.dart';
